1c1a851a6501d59fb4d9f77ca62b5cb46c493184
The Soapbox Squad <thesoapboxsquad@gmail.com>
2024-07-06 11:18:38 -0400

    archive events

--- archive.php
+++ archive.php
@@ -4,7 +4,8 @@
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title">
-           <?php 
+          <?php the_archive_title(); ?>
+           <!-- <?php 
             if(is_category()) {
                 single_cat_title();
                 
@@ -12,7 +13,7 @@
             if (is_author()) {
                 echo "Posts by "; the_author();
             }
-           ?>
+           ?> -->
         </h1>
         <div class="page-banner__intro">
           <!-- <p>Keep up with our latest news</p> -->
